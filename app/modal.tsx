45a41cd3c4169fd4f5cbaf4728b70b3a2f31188e
CitalKishan <kishan@cital.co>
2025-05-14 22:38:06 +0530

    modal 2 added

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import Modal1 from "../modals/modal1";
+import Modal2 from "../modals/modal2";
 const Modal = () => {
   console.log("Rendering Modal Screen");
   const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
       <TouchableOpacity
         onPress={() => setModalVisible(true)}
         style={{
@@ -26,7 +34,29 @@ const Modal = () => {
           Open Modal 1
         </Text>
       </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={{
+          backgroundColor: "#2196F3",
+          paddingVertical: 12,
+          paddingHorizontal: 20,
+          borderRadius: 8,
+          elevation: 3,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+        }}
+      >
+        <Text
+          style={{ color: "white", fontWeight: "bold", textAlign: "center" }}
+        >
+          Open Modal 2
+        </Text>
+      </TouchableOpacity>
+
       <Modal1 visible={modalVisible} onClose={() => setModalVisible(false)} />
+      <Modal2 visible={modalVisible} onClose={() => setModalVisible(false)} />
     </View>
   );
 };
